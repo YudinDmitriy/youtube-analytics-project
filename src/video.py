29ed1f06454e9e48ce756fe8e70b657865631151
Yudin Dmitriy <yudin01042002@yandex.ru>
2024-02-24 14:07:35 +0300

    4 домашка доработка

--- src/video.py
+++ src/video.py
@@ -24,23 +24,11 @@ class Video:
         print(json.dumps(self.video_id, indent=2, ensure_ascii=False))
 
 
-class PLVideo:
+class PLVideo(Video):
 
     def __init__(self, video_id: str, playlist_id: str):
-        self.video_id = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                              id=video_id).execute()
+        super().__init__(video_id)
         self.playlist_id = youtube.playlistItems().list(playlistId=playlist_id,
                                                         part='contentDetails', maxResults=50,).execute()
-        self.id = self.video_id['items'][0]['id']
-        self.title = self.video_id['items'][0]['snippet']['title']
-        self.url = f'https://youtube.com/watch/{self.video_id['items'][0]['id']}'
-        self.view_count = self.video_id['items'][0]['statistics']["viewCount"]
-        self.like_count = self.video_id['items'][0]['statistics']["likeCount"]
         self.id_pls = self.playlist_id['items'][0]['id']
 
-    def __str__(self):
-        return f'{self.title}'
-
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        print(json.dumps(self.playlist_id, indent=2, ensure_ascii=False))
